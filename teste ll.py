2c8c3c207bb79da8f71615199643e7d2f7ffdf8c
Jamily Gomes <jamilygomezg@gmail.com>
2024-09-20 17:09:12 -0400

    first commit

---
+++ teste ll.py
@@ -0,0 +1,73 @@
+#inserir nome
+#nome= input ("digite seu nome")
+#print(nome)
+
+
+#idade em string
+#idade= input ("digite sua idade")
+#print (idade)
+#print (type(idade))
+
+#salario em inteiro
+#salario= int(input("salario?="))
+#imposto= float(input("digite o imposto= print(type('esse e um string.'))")) 
+
+
+#sal= float(input("digite salario= "))
+#print(sal)
+
+#sal= int(input("salrio= "))
+#print ("o salario digitado foi %d" % sal)
+
+#strings (%s)
+#inteiro (%d)
+#boleano (%b)
+#float (%f)
+
+#obs. vc pode definir a quantidade de casas decimais adicionando, quantidadedecasas.
+#ex %2f
+
+
+#print ("a quantidade de %s é de %d, mas o salario é %f" % ("bananas", 10,1255,10))
+
+# idade1=10
+# idade2=12
+# filho=2
+# print ("\neu tenho %d filhos, ambos de %d e %d anos" % (filho, idade1, idade2))
+# print ("\neu tenho", filho, "filhos, ambos de ", idade1, "e", idade2, "anos")
+# print ("\neu tenho %d filhos, ambos de %d e %d anos" % (2,10,12))
+# print ("\neu tenho 2 filhos, ambos de 10 e 12 anos")
+# z = 1755/111
+
+# print ("%.4f" % 2)
+#%d - inteiro
+#%s - string
+#%f - float
+#%b -bool
+
+
+# a= "Hillary"
+# b= "Benson"
+# print ("Prezada " +a+ " " +b+  ". "  + "Olá!")
+
+# print(10*"Jamily\n")
+
+# print ("+" + 10 * "-" + "+")
+# print (("|" + " " * 10 + "|\n") * 5, end= "")
+# print ("+" + 10 * "-" + "+")
+
+# print(""*5 + "/" + "\ ")
+# print(""*4 + "/" + ""*2+"\ ")
+# print(""*3 + "/" + ""*4+"\ ")
+# print(""*2 + "/" + ""*6+"\ ")
+# print(""*1 + "/" + ""*8+"\ ")
+# print("/" + 10* "_" + "\ ")
+
+#frase= "um triangulo de base igual a {0}' a altura igual a {1} possui area igual a {2}.". format(3,4,12)
+#print(frase)
+
+#print(f"texto{variavel}")
+# linguagem= "python"
+# f"progamando em {linguagem}"
+# print(f"progamando em {linguagem}") 
+
---
+++ teste.py
@@ -0,0 +1,43 @@
+print("hello, word!!")
+print(2+3)
+print("dia", "mes", "ano", sep="/") 
+#sep indica o separador q vc escolher
+
+print(3.14)
+print(type(3.14))
+print(42000)
+print(42,000)
+print(42.000)
+print(type(42000))
+print(type(42.000))
+print(type(42))
+print(type("10"+"10")) 
+#é uma string, ñ conta como numeros
+
+# print(type('esse e um string.'))
+# print(type("esse tbm e um string."))
+# print(type("""e esse tbm."""))
+# print(type('''e msm esse.'''))
+
+mensagem = "nao contavam com minha astucia"
+n = 2021
+pi = 3.14159
+print (mensagem)
+print (type(mensagem))
+print (n)
+print (type(n))
+print (pi)
+print(type(pi))
+
+tipo = True
+print(type(tipo))
+#verdadeiro
+
+#sinal de igual = (um atribui) == (dois igual)
+
+print(81**0.5)
+
+peso = 45
+altura = 1.45
+
+print(45/1.45)
