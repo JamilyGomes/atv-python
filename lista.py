97da2ae0a36bd9a368db9740045439befbe1a97a
Jamily Gomes <jamilygomezg@gmail.com>
2024-10-01 17:21:53 -0400

    x

---
+++ lista.py
@@ -0,0 +1,51 @@
+# lista= "Jamily"
+# lista2=["jamily", 37, 1.75, 78.00]
+# print(lista, lista2)
+
+# lista1= [10, 20, 30, 40]
+# lista2= ["spam", "bungee", "swallow"]
+# print (lista1, lista2)
+
+# #sublist
+# lista1= ["oi", 2.0, 5, [10, 20]]
+# print (lista1)
+
+# lista1= ["oi", 2.0, 5, [10,20]]
+# print(len(lista1))
+
+# numeros= [1, 2, 3, 4, 5, 6, 7]
+# print(numeros[2])
+# print(numeros[9-8])
+# print(numeros[-2])
+# print(numeros[-1])
+
+# #imprimir o elemento, o objeto dentro do elemento e o caractere dentro do objeto
+# #DIREITA P ESQUERDA SE CONTA A PARTIR DO 0 E ESQUERDA PRA DIREITA SE CONTA A PARTIR DO -1
+
+# lista=[3, 7, "gato", [56, 57, "cachorro"], [ ], 3.14, "false"]
+# print(lista [2][0])
+
+# frutas= ["maça", "laranja", "banana", "cereja"]
+# print([1, 2] + [3, 4])
+# print(frutas + [6, 7, 8, 9])
+
+# print([] * 4)
+# print([1, 2, ["ola", "adeus"]] * 2)
+
+# #soma
+# a= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print (a)
+# print(max(a))
+# print(min(a))
+# print(sum(a))
+
+# a= [1, 2, 3, 4]
+# print(a[0:4])
+
+lista= ["a", "b", "c", "d", "e", "f"]
+print(lista[1:3])
+print(lista[:4])
+print(lista[3:])
+print(lista[:])
+print(lista[0:6])
+
