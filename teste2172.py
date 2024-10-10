a8c57591ddfcf3af191a31c752121c463c20e441
Jamily Gomes <jamilygomezg@gmail.com>
2024-10-10 17:05:43 -0400

    aula1010

--- teste2172.py
+++ teste2172.py
@@ -1,20 +1,20 @@
-def main():
-    while True:
+
+while True:
 #ler os valores x e m
-x, m= map(int, input().split())
+        x, m= map(int, input().split())
 
 #verificar a condição de parada
-if x == 0 and m==0:
-    break
+        if x == 0 and m == 0:
+
 
 #calcula o novo valor de exp
-e = m * x
+            e = x * m
 
-#imprime o resultado
-print(e)
+            #imprime o resultado
+            print(e)
 
-if name == "main":
-    main()
+# if name == "main":
+#     main()
 
 
 
